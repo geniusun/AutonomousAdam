7d70e4a4f101784722bb07626c9dc018622a1d3d
Geniusun <156289565@qq.com>
2023-11-14 05:26:02 +0800

    Update invitation_code_api.py
    change port

--- invitation_code_api.py
+++ invitation_code_api.py
@@ -47,4 +47,4 @@ invitations.update(generate_invitation_codes(5))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
